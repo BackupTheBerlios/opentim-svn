cba413c1aa168d8bf61242a9bec280ef31bc8a0f
lsaffre <lsaffre@6f5755b5-553f-0410-903e-f94a16489a43>
2008-12-24 00:11:42 +0000

    m

--- trunk/docs/conf.py
+++ trunk/docs/conf.py
@@ -187,20 +187,22 @@ latex_documents = [
 
 #language="de"
 
-sfile_base_uri="http://svn.berlios.de/wsvn/opentim/trunk"
+srcref_base_uri="http://svn.berlios.de/wsvn/opentim/trunk"
 
 from docutils import nodes, utils
 import posixpath
 
-def sfile_role(typ, rawtext, etext, lineno, 
+def srcref_role(typ, rawtext, etext, lineno, 
                inliner, options={}, content=[]):
     env = inliner.document.settings.env
-    baseuri = env.config.sfile_base_uri
+    baseuri = env.config.srcref_base_uri
     text = utils.unescape(etext)
     refnode = nodes.reference('', '', refuri=posixpath.join(baseuri, text))
     refnode += nodes.literal(text, text)
     return [refnode], []
 
 def setup(app):
-    app.add_role('sfile', sfile_role)
-    app.add_config_value('sfile_base_uri', 'http://example.com/source', True) 
+    app.add_role('srcref', srcref_role)
+    app.add_config_value('srcref_base_uri', 
+      'http://example.com/source', True)
+    app.add_description_unit('file','xfile','pair: %s; file')
--- trunk/docs/functions.rst
+++ trunk/docs/functions.rst
@@ -48,11 +48,16 @@ Global functions
   
   
 
-.. describe:: runbg.log
+.. file:: RUNBG.LOG
+
+  Log file created on each :func:`runbg` call which contains the stdout and stderr of the process. 
+
+.. file:: AUSLAND.DBC
 
   blablablabl
 
-.. describe:: ausland.dbc
+.. file:: DLM/SPRL/TOTAL.INC
 
-  blablablabl
-
+  To be included by the invoice template using ``[=fparse("TOTAL.INC")]``.
+  See :srcref:`DLM/SPRL/TOTAL.INC`.
+  
